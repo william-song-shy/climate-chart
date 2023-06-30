9fccbd34ba0a57f7a4ee4a7d3cf7f3baa93fb236
William Song <songhongyishy@gmail.com>
2023-06-30 16:04:58 +0800

    Format

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask,request,jsonify,send_file
+from flask import Flask, request, jsonify, send_file
 from main import gen_plot
 import base64
 from io import BytesIO
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     data = request.json
     name = data["name"]
     data = data["data"]
-    fig = gen_plot(data,name)
+    fig = gen_plot(data, name)
     buffer = BytesIO()
-    fig.savefig(buffer,format="png",dpi=300)
+    fig.savefig(buffer, format="png", dpi=300)
     buffer.seek(0)
     data = base64.b64encode(buffer.read()).decode("utf-8")
-    return send_file(BytesIO(base64.b64decode(data)),mimetype="image/png")
+    return send_file(BytesIO(base64.b64decode(data)), mimetype="image/png")
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from math import ceil,floor
+from math import ceil, floor
 
 from matplotlib import pyplot as plt
 from matplotlib.ticker import FuncFormatter, LinearLocator, FixedLocator
 
-data_test = {"prep":[54,46,50,44,58,56,53,51,56,57,58,54],"temp":[3.7,4.6,8.1,11.2,14.7,17.8,19.8,19.4,16.7,12,7.5,4.6]}
+data_test = {"prep": [54, 46, 50, 44, 58, 56, 53, 51, 56, 57, 58, 54],
+             "temp": [3.7, 4.6, 8.1, 11.2, 14.7, 17.8, 19.8, 19.4, 16.7, 12, 7.5, 4.6]}
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 
@@ -12,8 +13,7 @@ def month_formatter(x, pos):
         return months[int(x)]
 
 
-def gen_plot(data,name=""):
-
+def gen_plot(data, name=""):
     fig, ax1 = plt.subplots()
     ax1.xaxis.set_major_formatter(FuncFormatter(month_formatter))
     ax1.xaxis.set_ticks([x for x in range(12) if x % 3 == 1])
@@ -22,7 +22,7 @@ def gen_plot(data,name=""):
     ax1.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: str(int(x)) + " mm"))
     ax2.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: str(int(x)) + " °C"))
     ax1.bar(months, data["prep"], color="deepskyblue")
-    ax2.plot(months, data["temp"], color="darkred",clip_on=False)
+    ax2.plot(months, data["temp"], color="darkred", clip_on=False)
     ax1.set_ylim(0, ceil(max(data["prep"]) * 1.5 / 50) * 50)
     ax1.yaxis.set_major_locator(LinearLocator(numticks=6))
     diff = max(data["temp"]) - min(data["temp"])
@@ -30,12 +30,12 @@ def gen_plot(data,name=""):
     ma = max(data["temp"])
     mi = floor(mi / 5) * 5
     ma = ceil(ma / 5) * 5
-    if ma-mi <= 30:
-        ax2.set_ylim(ma-25, ma)
-        ax2.yaxis.set_major_locator(FixedLocator([ma-25,ma-20,ma-15,ma-10,ma-5,ma]))
+    if ma - mi <= 30:
+        ax2.set_ylim(ma - 25, ma)
+        ax2.yaxis.set_major_locator(FixedLocator([ma - 25, ma - 20, ma - 15, ma - 10, ma - 5, ma]))
     else:
         ax2.set_ylim(-10, 40)
-        ax2.yaxis.set_major_locator(FixedLocator([-10,0,10,20,30,40]))
+        ax2.yaxis.set_major_locator(FixedLocator([-10, 0, 10, 20, 30, 40]))
     ax1.grid(axis="y", color="black", linestyle="--")
     ax2.grid(axis="y", color="black", linestyle="--")
     ax1.set_title(f"Climate Chart - {name}")
@@ -43,6 +43,7 @@ def gen_plot(data,name=""):
     # plt.show()
     return fig
 
-if __name__=="__main__":
-    fig = gen_plot(data_test,'Paris')
-    fig.savefig("test.png",dpi=300)
+
+if __name__ == "__main__":
+    fig = gen_plot(data_test, 'Paris')
+    fig.savefig("test.png", dpi=300)
